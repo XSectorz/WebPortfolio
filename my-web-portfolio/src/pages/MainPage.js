5cd2fe6c017a233efbff28cdb83198de7434460d
XSectorz <panatschool@gmail.com>
2023-12-05 02:13:57 +0700

    Added fixed navbar

--- my-web-portfolio/src/pages/MainPage.js
+++ my-web-portfolio/src/pages/MainPage.js
@@ -6,7 +6,6 @@ import { FaLaptopCode } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import { IoIosMail,IoLogoLinkedin   } from "react-icons/io";
 import { MdOutlineKeyboardDoubleArrowUp  } from "react-icons/md";
-import { FaFacebookF } from "react-icons/fa6";
 
 import { FaChevronLeft,FaChevronRight } from "react-icons/fa";
 import '../css/animation.css'
@@ -113,60 +112,62 @@ export default function MainPage() {
 
     return(
         <div className='flex flex-col bg-black h-full'>
-            <div className='flex flex-col justify-center mt-8'>
-                <div className='flex mx-96'>
-                    <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 1 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
-                    onMouseEnter={() => hoverMenu(1)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        01
-                    </button >
-                    <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 2 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
-                    onMouseEnter={() => hoverMenu(2)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        02
-                    </button >
-                    <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 3 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
-                    onMouseEnter={() => hoverMenu(3)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        03
-                    </button >
-                    <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 4 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
-                    onMouseEnter={() => hoverMenu(4)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        04
-                    </button >
-                    <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 5 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
-                    onMouseEnter={() => hoverMenu(5)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        05
-                    </button >
-                </div>
-                <div className='flex mx-96'>
-                <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 1 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
-                    onMouseEnter={() => hoverMenu(1)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        // home
-                    </button >
-                    <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 2 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
-                    onMouseEnter={() => hoverMenu(2)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        // education
-                    </button >
-                    <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 3 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
-                    onMouseEnter={() => hoverMenu(3)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        // projects
-                    </button >
-                    <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 4 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
-                    onMouseEnter={() => hoverMenu(4)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        // experience
-                    </button >
-                    <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 5 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
-                    onMouseEnter={() => hoverMenu(5)}
-                    onMouseLeave={() => hoverMenu(0)}>
-                        // contact me
-                    </button >
+            <div className='flex bg-[#272727] w-full fixed' style={{ zIndex: 2 }}>
+                <div className='flex flex-col justify-center py-2 w-full'>
+                    <div className='flex mx-96'>
+                        <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 1 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
+                        onMouseEnter={() => hoverMenu(1)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            01
+                        </button >
+                        <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 2 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
+                        onMouseEnter={() => hoverMenu(2)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            02
+                        </button >
+                        <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 3 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
+                        onMouseEnter={() => hoverMenu(3)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            03
+                        </button >
+                        <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 4 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
+                        onMouseEnter={() => hoverMenu(4)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            04
+                        </button >
+                        <button  className={`flex w-1/5 text-sm font-mono font-bold mx-6 justify-end mt-2 ${ hoverIndex === 0 || hoverIndex === 5 ? 'text-slate-200 duration-300' : 'text-gray-500 duration-300'}`}
+                        onMouseEnter={() => hoverMenu(5)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            05
+                        </button >
+                    </div>
+                    <div className='flex mx-96'>
+                    <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 1 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
+                        onMouseEnter={() => hoverMenu(1)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            // home
+                        </button >
+                        <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 2 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
+                        onMouseEnter={() => hoverMenu(2)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            // education
+                        </button >
+                        <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 3 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
+                        onMouseEnter={() => hoverMenu(3)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            // projects
+                        </button >
+                        <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 4 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
+                        onMouseEnter={() => hoverMenu(4)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            // experience
+                        </button >
+                        <button  className={`flex w-full text-lg font-mono font-bold mx-6 justify-end ${ hoverIndex === 0 || hoverIndex === 5 ? 'text-white duration-500' : 'text-gray-500 duration-500'}`}
+                        onMouseEnter={() => hoverMenu(5)}
+                        onMouseLeave={() => hoverMenu(0)}>
+                            // contact me
+                        </button >
+                    </div>
                 </div>
             </div>
             <div className='items-center justify-center mt-8'>
